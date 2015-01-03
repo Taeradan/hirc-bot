e3779e3321ff0d1d15a3913289d5cf27a16a2954
Yves Dubromelle <taeradan@dubronetwork.fr>
2015-01-03 22:19:43 +0100

    noms de variables plus explicites

--- HIRC.hs
+++ HIRC.hs
@@ -14,12 +14,12 @@ nick   = "haskell-bot"
 -- | Fonction principale, se connecte au serveur IRC et lance la boucle d'écoute
 main :: IO ()
 main = do
-        h <- connectTo server (PortNumber (fromIntegral port))
-        hSetBuffering h NoBuffering
-        write h "NICK" nick
-        write h "USER" (nick ++ " 0 * :tutorial bot")
-        write h "JOIN" chan
-        listen h
+        handle <- connectTo server (PortNumber (fromIntegral port))
+        hSetBuffering handle NoBuffering
+        write handle "NICK" nick
+        write handle "USER" (nick ++ " 0 * :tutorial bot")
+        write handle "JOIN" chan
+        listen handle
 
 {-|
     Boucle d'écoute du serveur.
@@ -29,29 +29,29 @@ main = do
     * Lancer le traitement des commandes
 -}
 listen :: Handle -> IO ()
-listen h = forever $ do
-        t <- hGetLine h
-        let s = init t
-        if ping s then pong s else eval h (clean s)
-        putStrLn s
+listen handle = forever $ do
+        line <- hGetLine handle
+        let command = init line
+        if ping command then pong command else eval handle (clean command)
+        putStrLn command
             where
                clean     = drop 1 . dropWhile ( /= ':') . drop 1
                ping x    = "PING :" `isPrefixOf` x
-               pong x    = write h "PONG" (':' : drop 6 x)
+               pong x    = write handle "PONG" (':' : drop 6 x)
 
 -- | Fonction qui évalue une commande IRC
 eval :: Handle -> String -> IO ()
-eval h    "!quit"                = write h "QUIT" ":Exiting" >> exitSuccess
-eval h x | "!id " `isPrefixOf` x = privmsg h (drop 4 x)
+eval handle    "!quit"                = write handle "QUIT" ":Exiting" >> exitSuccess
+eval handle x | "!id " `isPrefixOf` x = privmsg handle (drop 4 x)
 eval _   _                       = return () -- ignore everything else
 
 -- | Fonction qui envoie un message sur le chan
 privmsg :: Handle -> String -> IO ()
-privmsg h s = write h "PRIVMSG" (chan ++ " :" ++ s)
+privmsg handle string = write handle "PRIVMSG" (chan ++ " :" ++ string)
 
 -- | Fonction de base qui envoie au serveur une commande IRC
 write :: Handle -> String -> String -> IO ()
-write h s t = do
-        hPrintf h "%s %s\r\n" s t
+write handle s t = do
+        hPrintf handle "%s %s\r\n" s t
         printf    "> %s %s\n" s t
 
